f2b8f6f8b53d862b7caf5ec48e9ea6baa7b92b2c
nhattinh2601 <nhattinh2601@gmail.com>
2024-02-07 16:56:58 +0700

    up

--- App.js
+++ App.js
@@ -4,35 +4,77 @@
  *
  * @format
  */
-
 import React from 'react';
-import {View, Text, StatusBar} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from './src/components/Home/Home';
+import Ionic from 'react-native-vector-icons/Ionicons';
+import MyTickets from './src/components/MyTickets/MyTickets';
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  const BottomTabs = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: '#1A1A23',
+            borderTopEndRadius: 10,
+            borderTopStartRadius: 10,
+            position: 'absolute',
+            borderTopColor: 'transparent',
+            elevation: 0,
+            height: 54,
+            overflow: 'hidden',
+          },
+          tabBarIcon: ({focused, colour}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused ? 'home-sharp' : 'home-outline';
+              colour = focused && '#ffffff';
+            } else if (route.name === 'MyTickets') {
+              iconName = focused ? 'film' : 'film-outline';
+              colour = focused && '#ffffff';
+            }
+            return (
+              <>
+                <Ionic
+                  name={iconName}
+                  style={{marginBottom: 4}}
+                  size={22}
+                  color={colour ? colour : '#ffffff40'}
+                />
+                <Ionic
+                  name="ellipse"
+                  style={{display: colour ? 'flex' : 'none'}}
+                  size={4}
+                  color={colour ? colour : 'transparent'}
+                />
+              </>
+            );
+          },
+        })}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="MyTickets" component={MyTickets} />
+      </Tab.Navigator>
+    );
+  };
 
   return (
-    <View
-      style={{
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <StatusBar backgroundColor="#ffffff" barStyle="dark-content" />
-      <Text
-        style={{
-          fontSize: 30,
-          color: '#000000',
-          fontWeight: 'bold',
-          letterSpacing: 1,
-        }}
-      >Welcome</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="BottomTabs" component={BottomTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-
+};
 
 export default App;
---
+++ src/components/MyTickets/MyTickets.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function MyTickets(){
+        return(
+                <View>
+                        <Text>MyTickets</Text>
+                </View>
+        );
+}
